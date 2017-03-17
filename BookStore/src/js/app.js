f6fb1cb4d0904d01eec81ee71ad8c3c7b8e7682e
cy0707 <674986077@qq.com>
2017-03-17 15:31:05 +0800

    bookstore系统的路由理顺了

--- BookStore/src/js/app.js
+++ BookStore/src/js/app.js
@@ -1,6 +1,14 @@
-var bookStoreApp = angular.module('bookStoreApp', [
-	'ui.router', 'ngGrid', 'BookListModule', 'BookDetailModule'
-]);
+/**
+ * bookStoreApp是项目的入口模块，其依赖ui.router ui.grid
+ * 以及我们自定义的两个模块，BookListModule和BookDetailModule
+ */
+
+
+
+var bookStoreApp = angular.module('bookStoreApp', ['ui.router']);
+// , [
+//     'ui.router', 'ui.grid', 'BookListModule', 'BookDetailModule'
+// ]
 
 /**
  * 由于整个应用都会和路由打交道，所以这里把$state和$stateParams这两个对象放到$rootScope上，方便其它地方引用和注入。
@@ -33,39 +41,50 @@ bookStoreApp.run(function($rootScope, $state, $stateParams){
  * @return {[type]}
  */
 bookStoreApp.config(function($stateProvider, $urlRouterProvider) {
+    // 如果没有匹配到其他路由，则直接返回到首页
     $urlRouterProvider.otherwise('/index');
+    // 路由的而配置
     $stateProvider
         .state('index', {
+            // 如果是首页
             url: '/index',
             views: {
                 '': {
+                // 为空的话就是主页
                     templateUrl: 'tpls/home.html'
                 },
                 'main@index': {
-                    templateUrl: 'tpls/loginForm.html'
+                    // 如果有嵌套路由的话，就是注册页
+                    templateUrl: 'tpls/login_form.html'
                 }
             }
         })
         .state('booklist', {
+            // 书的列表
             url: '/{bookType:[0-9]{1,4}}',
             views: { //注意这里的写法，当一个页面上带有多个ui-view的时候如何进行命名和视图模板的加载动作
                 '': {
-                    templateUrl: 'tpls/bookList.html'
+                // 主页是booklist
+                    templateUrl: 'tpls/booklist.html'
                 },
                 'booktype@booklist': {
-                    templateUrl: 'tpls/bookType.html'
+                    // booktype的路由为booktype的模板
+                    templateUrl: 'tpls/book_type.html'
                 },
                 'bookgrid@booklist': {
-                    templateUrl: 'tpls/bookGrid.html'
+                    // bookgrid的路由为bookgrid的模板
+                    templateUrl: 'tpls/bookgrid.html'
                 }
             }
         })
         .state('addbook', {
+            // 添加书籍
             url: '/addbook',
-            templateUrl: 'tpls/addBookForm.html'
+            templateUrl: 'tpls/addbook_form.html'
         })
         .state('bookdetail', {
+            // 书的详细
             url: '/bookdetail/:bookId', //注意这里在路由中传参数的方式
-            templateUrl: 'tpls/bookDetail.html'
+            templateUrl: 'tpls/book_details.html'
         })
 });
--- BookStore/src/js/controllers.js
+++ BookStore/src/js/controllers.js
@@ -1,29 +1,149 @@
-var bookStoreCtrls = angular.module("bookStoreCtrls", []);
+/**
+ * 这里是书籍列表模块
+ * @type {[type]}
+ */
 
-// controller
-bookStoreCtrls.controller("helloCtrl", ["$scope", function($scope){
-	$scope.greeting = {
-		text: "hello",
+var booklistModule = angular.module('BookListCtrl', []);
+booklistModule.controller('BookListCtrl', ['$scope', '$http', '$state', '$statePara','ui.grid.pagination',
+ function($scope, $http, $state, $statePara, ui.grid.pagination){
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//ui-grid
+ $scope.gridOptions = {
+    data: 'myData',
+    columnDefs: [{ field: 'index', 
+                   displayName: '序号', 
+                   width: '10%', 
+                   enableColumnMenu: false,// 是否显示列头部菜单按钮
+                   enableHiding: false,
+                   suppressRemoveSort: true,
+                   enableCellEdit: false // 是否可编辑
+                 },
+                 { field: "name",
+                   displayName: '书名'
+             	 },
+                { field: "author",
+                  displayName: '作者'
+            	 },
+                { field: "publicTime",
+                  displayName: '出版日期'
+            	 },
+                { field: "price",
+                  displayName: '定价',
+                  cellFilter: 'currency:"￥"'
+            	 },	 
+                { field: "bookId",
+                  displayName: '操作'
+            	 } 	 
+                 
+                ],
+                
+    enableSorting: true, //是否排序
+    useExternalSorting: false, //是否使用自定义排序规则
+    enableGridMenu: true, //是否显示grid 菜单
+    showGridFooter: true, //是否显示grid footer
+    enableHorizontalScrollbar :  0, //grid水平滚动条是否显示, 0-不显示  1-显示
+    enableVerticalScrollbar : 0, //grid垂直滚动条是否显示, 0-不显示  1-显示
+    
+    //-------- 分页属性 ----------------
+    enablePagination: true, //是否分页，默认为true
+    enablePaginationControls: true, //使用默认的底部分页
+    paginationPageSizes: [10, 15, 20], //每页显示个数可选项
+    paginationCurrentPage:1, //当前页码
+    paginationPageSize: 10, //每页显示个数
+    //paginationTemplate:"<div></div>", //自定义底部分页代码
+    totalItems : 0, // 总数量
+    useExternalPagination: true,//是否使用分页按钮
+    
+
+    //----------- 选中 ----------------------
+    enableFooterTotalSelected: true, // 是否显示选中的总数，默认为true, 如果显示，showGridFooter 必须为true
+    enableFullRowSelection : true, //是否点击行任意位置后选中,默认为false,当为true时，checkbox可以显示但是不可选中
+    enableRowHeaderSelection : true, //是否显示选中checkbox框 ,默认为true
+    enableRowSelection : true, // 行选择是否可用，默认为true;
+    enableSelectAll : true, // 选择所有checkbox是否可用，默认为true; 
+    enableSelectionBatchEvent : true, //默认true
+       isRowSelectable: function(row){ //GridRow
+       if(row.entity.age > 45){
+           row.grid.api.selection.selectRow(row.entity); // 选中行
+       }
+    },
+    modifierKeysToMultiSelect: false ,//默认false,为true时只能 按ctrl或shift键进行多选, multiSelect 必须为true;
+    multiSelect: true ,// 是否可以选择多个,默认为true;
+    noUnselect: false,//默认false,选中后是否可以取消选中
+    selectionRowHeaderWidth:30 ,//默认30 ，设置选择列的宽度；
+  
+    //---------------api---------------------
+    onRegisterApi: function(gridApi) {
+        $scope.gridApi = gridApi;
+        //分页按钮事件
+        gridApi.pagination.on.paginationChanged($scope,function(newPage, pageSize) {
+              if(getPage) { 
+                  getPage(newPage, pageSize); 
+               }
+        });
+        //行选中事件
+        $scope.gridApi.selection.on.rowSelectionChanged($scope,function(row,event){
+            if(row){
+                $scope.testRow = row.entity;
+            }
+         });
+    }
+};
+
+	var getPage = function(curPage, pageSize) {
+	        var firstRow = (curPage - 1) * pageSize;
+	        $scope.gridOptions.totalItems = mydefalutData.length;
+	        $scope.gridOptions.data = mydefalutData.slice(firstRow, firstRow + pageSize);
+	        //或者像下面这种写法
+	        //$scope.myData = mydefalutData.slice(firstRow, firstRow + pageSize);
 	};
 
-	$scope.pageClass = "hello";
+	var mydefalutData = [{ name: "Moroni", age: 50, birthday: "Oct 28, 1970", salary: "60,000" },
+	                { name: "Tiancum", age: 43, birthday: "Feb 12, 1985", salary: "70,000" },
+	                { name: "Jacob", age: 27, birthday: "Aug 23, 1983", salary: "50,000" },
+	                { name: "Nephi", age: 29, birthday: "May 31, 2010", salary: "40,000" },
+	                { name: "Enos", age: 34, birthday: "Aug 3, 2008", salary: "30,000" },
+	                { name: "Moroni", age: 50, birthday: "Oct 28, 1970", salary: "60,000" },
+	                { name: "Tiancum", age: 43, birthday: "Feb 12, 1985", salary: "70,000" },
+	                { name: "Jacob", age: 27, birthday: "Aug 23, 1983", salary: "40,000" },
+	                { name: "Nephi", age: 29, birthday: "May 31, 2010", salary: "50,000" },
+	                { name: "Enos", age: 34, birthday: "Aug 3, 2008", salary: "30,000" },
+	                { name: "Moroni", age: 50, birthday: "Oct 28, 1970", salary: "60,000" },
+	                { name: "Tiancum", age: 43, birthday: "Feb 12, 1985", salary: "70,000" },
+	                { name: "Jacob", age: 27, birthday: "Aug 23, 1983", salary: "40,000" },
+	                { name: "Nephi", age: 29, birthday: "May 31, 2010", salary: "50,000" },
+	                { name: "Enos", age: 34, birthday: "Aug 3, 2008", salary: "30,000" }];
 
-}]);
+	getPage(1, $scope.gridOptions.paginationPageSize);
+	
 
-
-bookStoreCtrls.controller("bookListCtrl", ["$scope", function($scope){
-	$scope.books = [{
-		title: "《this is a one》",
-		author: "one"
-	},
-	{
-		title: "《this is a two》",
-		author: "two"
-	},
-	{
-		title: "《this is a three》",
-		author: "three"
-	}];
-
-	$scope.pageClass = "list";
 }]);
