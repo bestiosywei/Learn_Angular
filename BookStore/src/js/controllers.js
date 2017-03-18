6f9f98362225b5d85322ece01593f448a3a88bcb
cy0707 <674986077@qq.com>
2017-03-18 23:52:52 +0800

    整体的思路已经理解清楚

--- BookStore/src/js/controllers.js
+++ BookStore/src/js/controllers.js
@@ -5,70 +5,119 @@
 
 var BookListModule = angular.module('BookListModule', []);
 BookListModule.controller('BookListCtrl', ['$scope', '$http', '$state', '$stateParams',
- function($scope, $http, $state, $stateParas){
+ function($scope, $http, $state, $stateParams){
+
+//这里可以根据路由上传递过来的bookType参数加载不同的数据
+/**
+ * [getPagedDataAsync 获取异步数据]
+ * @param  {[type]} pageSize   [每一页的数据个数]
+ * @param  {[type]} page       [当前页的编号]
+ * @param  {[type]} searchText [搜索的关键字]
+ */
+
+  $scope.getPagedDataAsync = function(pageSize, page, searchText) {
+      setTimeout(function() {
+          var data;
+          if (searchText) {
+            // 搜索文本
+              var ft = searchText.toLowerCase();
+              // 得到json文件
+              $http.get('data/books' + $stateParams.bookType + '.json')
+                  .then(function(response) {
+                    // 成功的话
+                      data = response.data.filter(function(item) {
+                        // 如果找到的话，返回true
+                          return JSON.stringify(item).toLowerCase().indexOf(ft) != -1;
+                      });
+                      $scope.setPagingData(data, page, pageSize);
+                  }, function(){
+                    console.log('http的get请求失败' + response.status);
+                  });
+          } else {
+              $http.get('data/books' + $stateParams.bookType + '.json')
+                  .then(function(response) {
+                      $scope.setPagingData(response.data, page, pageSize);
+                  }, function(){
+                    console.log('http的get请求失败' + response.status);
+                  });
+          }
+      }, 100);
+  };
+
+
+  var paginationOptions = {
+     pageNumber: 1,
+     pageSize: 5,
+     sort: null
+   };
+
+/**
+ * [setPagingData description]
+ * @param {[type]} data     [祸胎获取的数据]
+ * @param {[type]} page     [当前页的编号]
+ * @param {[type]} pageSize [每一个的数据的个数]
+ */
+  $scope.setPagingData = function(data, page, pageSize) {
+    // 获取当前页的数据总数  当前页的总数据条数，赋值给ui-grid的data属性
+    var firstRow = (page - 1) * pageSize;
+    $scope.gridOptions.data = data.slice(firstRow, firstRow + pageSize);
+      // 从后台获取的全部数据的条数
+     $scope.gridOptions.totalItems = data.length;
+  };
+
+//从ui-gird从获取页数pageSize currentPage当前页
+  $scope.getPagedDataAsync(paginationOptions.pageNumber,paginationOptions.pageSize);
 	
-//ui-grid
- $scope.gridOptions = {
-    data: 'books',
-    columnDefs: [{ field: 'index', 
-                   displayName: '序号', 
-                   width: '10%', 
-                   enableColumnMenu: false,// 是否显示列头部菜单按钮
-                   enableHiding: false,
-                   suppressRemoveSort: true,
-                   enableCellEdit: false // 是否可编辑
-                 },
-                 { field: "name",
-                   displayName: '书名'
-             	 },
-                { field: "author",
-                  displayName: '作者'
-            	 },
-                { field: "publicTime",
-                  displayName: '出版日期'
-            	 },
-                { field: "price",
-                  displayName: '定价',
-                  cellFilter: 'currency:"￥"'
-            	 },	 
-                { field: "bookId",
-                  displayName: '操作'
-            	 } 	 
-                ],
-                
-    enableSorting: true, //是否排序
-    useExternalSorting: false, //是否使用自定义排序规则
-    enableGridMenu: true, //是否显示grid 菜单
-    showGridFooter: true, //是否显示grid footer
-    enableHorizontalScrollbar :  0, //grid水平滚动条是否显示, 0-不显示  1-显示
-    enableVerticalScrollbar : 0, //grid垂直滚动条是否显示, 0-不显示  1-显示
-    
-    //-------- 分页属性 ----------------
-    enablePagination: true, //是否分页，默认为true
-    enablePaginationControls: true, //使用默认的底部分页
-    paginationPageSizes: [10, 15, 20], //每页显示个数可选项
-    paginationCurrentPage:1, //当前页码
-    paginationPageSize: 10, //每页显示个数
-    //paginationTemplate:"<div></div>", //自定义底部分页代码
-    totalItems : 0, // 总数量
-    useExternalPagination: true,//是否使用分页按钮
-    //---------------api---------------------
-    onRegisterApi: function(gridApi) {
-        $scope.gridApi = gridApi;
-        //分页按钮事件
-        gridApi.pagination.on.paginationChanged($scope,function(newPage, pageSize) {
-              if(getPage) { 
-                  getPage(newPage, pageSize); 
-               }
-        });
-        //行选中事件
-        $scope.gridApi.selection.on.rowSelectionChanged($scope,function(row,event){
-            if(row){
-                $scope.testRow = row.entity;
-            }
-         });
-    }
-};
+//利用后台数据ui-grid,填充表格
+     $scope.gridOptions = {
+        columnDefs: [{ field: 'index', 
+                       displayName: '序号', 
+                       width: '10%', 
+                       enableColumnMenu: false,// 是否显示列头部菜单按钮
+                       enableHiding: false,
+                       suppressRemoveSort: true,
+                       enableCellEdit: false // 是否可编辑
+                     },
+                     { field: "name",
+                       displayName: '书名'
+                 	 },
+                    { field: "author",
+                      displayName: '作者'
+                	 },
+                    { field: "publicTime",
+                      displayName: '出版日期'
+                	 },
+                    { field: "price",
+                      displayName: '定价',
+                      cellFilter: 'currency:"￥"'
+                	 },	 
+                    { field: "bookId",
+                      displayName: '操作',
+                      cellTemplate: '<div><a ui-sref="bookdetail({bookId:row.getProperty(col.field)})" id="{{row.getProperty(col.field)}}">详情</a></div>'
+                    }
+                    ],
+                    
+        //-------- 分页属性 ----------------
+        enablePagination: true, //是否分页，默认为true
+        enablePaginationControls: true, //使用默认的底部分页
+        paginationPageSizes: [5, 10, 15], //每页显示个数可选项
+        paginationPageSize: 5, //每页显示个数
+        paginationCurrentPage:1, //当前页码
+        //paginationTemplate:"<div></div>", //自定义底部分页代码
+        totalItems : 0, // 总数量
+        useExternalPagination: true,//是否使用分页按钮
+        useExternalSorting: false, //是否使用自定义排序规则
+        //---------------api---------------------
+        // onRegisterApi: function(gridApi) {
+        //     $scope.gridApi = gridApi;
+        //     //分页按钮事件
+        //     gridApi.pagination.on.paginationChanged($scope,function(newPage, pageSize) {
+        //           if(getPage) { 
+        //               getPage(newPage, pageSize); 
+        //            }
+        //     });
+        // }
+    };
 
 
 }]);
